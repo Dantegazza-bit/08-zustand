a6998750cc47d6937a66fb9f0ac6c40df21f8437
Oleksandr <oleksandrzabolotny@gmail.com>
2025-12-07 08:09:42 +0100

    Fix filter Notes client and error files

---
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -0,0 +1,13 @@
+// app/notes/filter/[...slug]/Notes.client.tsx
+"use client";
+
+import NoteList from "@/components/NoteList/NoteList";
+import type { Note } from "@/types/note";
+
+interface NotesClientProps {
+  notes: Note[];
+}
+
+export default function NotesClient({ notes }: NotesClientProps) {
+  return <NoteList notes={notes} />;
+}
